0d3060d645f9b5f333a2d983b6853f31c405eca7
saroj raut <sarojraut@sarojs-MacBook-Pro.local>
2026-03-15 21:15:52 +0545

    changed vite base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/crude-oil-price/',
+  base: '/demo-price-tracker/',
   build: {
     rollupOptions: {
       output: {
